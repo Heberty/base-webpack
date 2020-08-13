4d14c0a9b1514c4e6cd33867f9784f361f2d99b1
Heberty Carlos <heberty@maxmail.com.br>
2020-08-12 22:57:10 -0300

    Teste finalizado

--- public/pages/layouts/blocks/_header.php
+++ public/pages/layouts/blocks/_header.php
@@ -1,3 +1,101 @@
-<header>
-    
+<header class="header-site">
+    <div class="header-top">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-12 d-flex justify-content-between">
+			    	<ul class="menu-page">
+			    		<li class="menu-page-item">
+			    			<a href="javascript:;" class="menu-page-item-link active">Para <span>Você</span></a>
+			    		</li>
+			    		<li class="menu-page-item">
+			    			<a href="javascript:;" class="menu-page-item-link">Para <span>Empresas</span></a>
+			    		</li>
+			    	</ul>
+			    	<ul class="list-access">
+			    		<li class="list-access-item">
+			    			<a href="javascript:;" class="list-access-item-button">+</a>
+			    		</li>
+			    		<li class="list-access-item">
+			    			<a href="javascript:;" class="list-access-item-button">A</a>
+			    		</li>
+			    		<li class="list-access-item">
+			    			<a href="javascript:;" class="list-access-item-button">-</a>
+			    		</li>
+			    	</ul>
+			    	<ul class="list-more-access">
+			    		<li class="list-more-access-item">
+			    			<a href="javascript" class="list-more-access-item-link">Contraste</a>
+			    		</li>
+			    		<li class="list-more-access-item">
+			    			<a href="javascript" class="list-more-access-item-link"><img src="/images/min/icon-access.png" alt="" class="icon-hand">
+			    			</a>
+			    		</li>
+			    		<li class="list-more-access-item">
+			    			<a href="javascript" class="list-more-access-item-link">Acessibilidade</a>
+			    		</li>
+			    	</ul>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+    <div class="header-bottom">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-12 d-flex justify-content-between align-items-center">
+    				<a href="/" class="logo"><span>RN</span>.edu</a>
+    				<ul class="list-menu">
+    					<li class="list-menu-item"><a href="javascript:;" class="list-menu-item-link">Institucional</a></li>
+    					<li class="list-menu-item"><a href="javascript:;" class="list-menu-item-link">Cursos</a></li>
+    					<li class="list-menu-item"><a href="javascript:;" class="list-menu-item-link">Pesquisa</a></li>
+    					<li class="list-menu-item"><a href="javascript:;" class="list-menu-item-link">Extensão</a></li>
+    				</ul>
+    				<a href="javascript:;" class="btn-site">Inscreva-se</a>
+					<div class="dropdown">
+						<a class="btn btn-secondary dropdown-toggle student-portal" href="#" role="button" id="portalDoAluno" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Portal do Aluno <img src="/images/min/down.svg" alt="">
+						</a>
+						<div class="dropdown-menu" aria-labelledby="portalDoAluno">
+								<ul class="list-dropdown">
+									<li class="list-dropdown-item">
+										<a href="javascript:;" class="list-dropdown-item-link">Meus cursos</a>
+									</li>
+									<li class="list-dropdown-item">
+										<a href="javascript:;" class="list-dropdown-item-link">Impressão de 2a. Via</a>
+									</li>
+									<li class="list-dropdown-item">
+										<a href="javascript:;" class="list-dropdown-item-link">Débito automático</a>
+									</li>
+									<li class="list-dropdown-item">
+										<a href="javascript:;" class="list-dropdown-item-link">Mudança de vencimento</a>
+									</li>
+									<li class="list-dropdown-item">
+										<a href="javascript:;" class="list-dropdown-item-link">Entenda sua fatura</a>
+									</li>
+									<li class="list-dropdown-item">
+										<a href="javascript:;" class="list-dropdown-item-link">Fatura por e-mail</a>
+									</li>
+								</ul>
+								<form action="" class="form-dropdown form-site">
+									<h1 class="form-dropdown-title">Olá!</h1>
+									<h2 class="form-dropdown-subtitle">Bem-vindo ao Portal do Aluno</h2>
+
+									<div class="form-group">
+										<div class="form-label">E-mail ou CPF</div>
+										<input type="text" class="form-control cpf">
+									</div>
+									<div class="form-group">
+										<div class="form-label">Senha</div>
+										<input type="password" class="form-control">
+									</div>
+									<div class="d-flex justify-content-between align-items-center">
+										<a href="javascript:;" class="link-remember"><img src="/images/min/down.svg" alt=""> Lembrar a senha</a>
+										<a href="" class="btn-site">Entrar</a>
+									</div>
+								</form>
+						</div>
+					</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
 </header>
